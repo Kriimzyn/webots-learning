85bc4fc5656411d5f1a241cd8a2a1d11a973c0aa
Kriimzyn <rusticvalve@gmail.com>
2025-11-22 20:03:48 -0800

    Add files via upload

---
+++ controllers/EPuckGoForward/EPuckGoForward.c
@@ -0,0 +1,70 @@
+/*
+ * File:          EPuckGoForward.c
+ * Date:
+ * Description:
+ * Author:
+ * Modifications:
+ */
+
+/*
+ * You may need to add include files like <webots/distance_sensor.h> or
+ * <webots/motor.h>, etc.
+ */
+#include <webots/motor.h>
+
+#define TIME_STEP 64
+
+#define MAX_SPEED 6.28
+
+/*
+ * You may want to add macros here.
+ */
+#include <webots/robot.h>
+ 
+#define TIME_STEP 64
+
+/*
+ * This is the main program.
+ * The arguments of the main function can be specified by the
+ * "controllerArgs" field of the Robot node
+ */
+int main(int argc, char **argv) {
+  
+  wb_robot_init();
+  //assign handler to the moters and set target
+  WbDeviceTag left_motor = wb_robot_get_device("left wheel motor");
+  WbDeviceTag right_motor = wb_robot_get_device("right wheel motor");
+  wb_motor_set_position(left_motor, INFINITY);
+  wb_motor_set_position(right_motor, INFINITY);
+  // set up motor speeds
+  wb_motor_set_velocity(left_motor, 0.1 * MAX_SPEED);
+  wb_motor_set_velocity(right_motor, 0.1 * MAX_SPEED);
+  
+  
+  
+  /* main loop
+   * Perform simulation steps of TIME_STEP milliseconds
+   * and leave the loop when the simulation is over
+   */
+  while (wb_robot_step(TIME_STEP) != -1) {
+    /*
+     * Read the sensors :
+     * Enter here functions to read sensor data, like:
+     *  double val = wb_distance_sensor_get_value(my_sensor);
+     */
+
+    /* Process sensor data here */
+
+    /*
+     * Enter here functions to send actuator commands, like:
+     * wb_motor_set_position(my_actuator, 10.0);
+     */
+  };
+
+  /* Enter your cleanup code here */
+
+  /* This is necessary to cleanup webots resources */
+  wb_robot_cleanup();
+
+  return 0;
+}
